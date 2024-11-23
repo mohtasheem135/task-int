8b55b59c5025a2508816d7a70d513161ad96c536
mohtasheem135 <mohtasheemejaz@gmail.com>
2024-11-23 19:50:42 +0530

    Added -env.local and saved the credentials there

--- src/lib/firebaseConfig.jsx
+++ src/lib/firebaseConfig.jsx
@@ -1,17 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getAuth, GoogleAuthProvider } from "firebase/auth"
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAfXHoq8ULC7V5D2BfI2VnjBEKBfYDJAA0",
-  authDomain: "auth-production-6d8c7.firebaseapp.com",
-  projectId: "auth-production-6d8c7",
-  storageBucket: "auth-production-6d8c7.firebasestorage.app",
-  messagingSenderId: "558793888634",
-  appId: "1:558793888634:web:536bd0c35d881236e3fcc3"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
